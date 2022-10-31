b061359683f90cb2aafaf1e331927738e7c98ddb
Chau Le <chau_le@mckinsey.com>
2022-10-31 19:31:09 +0700

    fe: add header

--- fe/src/pages/landingPage.tsx
+++ fe/src/pages/landingPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import EventContainer from '../components/EventContainer';
+import Header from '../components/Header';
 
 const LandingPage: React.FC<{}> = () => {
   return (
     <>
+      <Header />
       <EventContainer />
     </>
   );
